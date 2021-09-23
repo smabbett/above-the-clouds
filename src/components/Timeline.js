9e99f0daf11863df390acb47fbcdb57c22be2374
Sarah Abbett <sarah.abbett@gmail.com>
2021-09-22 23:22:11 -0500

    added overflow scrolling, width to timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,22 +8,20 @@ export default function Timeline({ rotations }) {
 	for (let [key, value] of rotations.entries()) {
 		let rotNumber = 'Rotation ' + value.rotation.toString();
 		let startDate = new Date(key);
-		//new Date(year, month, date, hours, minutes, seconds, ms)
-		//startDate.setHours(0, 0, 0);
-		let startYear = startDate.getFullYear();
-		let startMonth = startDate.getMonth();
-		let startDay = startDate.getDate();
-		console.log(`(${startYear}, ${startMonth}, ${startDay}, 0, 0, 0, 0)`);
-		let start = new Date(startYear, startMonth, startDay, 0, 0, 0, 0);
 
-		console.log('start', start);
+		startDate.setHours(0, 0, 0);
+		// let startYear = startDate.getFullYear();
+		// let startMonth = startDate.getMonth();
+		// let startDay = startDate.getDate();
+		// console.log(`(${startYear}, ${startMonth}, ${startDay}, 0, 0, 0, 0)`);
+		// let start = new Date(startYear, startMonth, startDay, 0, 0, 0, 0);
+
 		let endDate = new Date(value.segments[value.segments.length - 1].dept_date);
-		//endDate.setHours(24, 0, 0);
-		let endYear = endDate.getFullYear();
-		let endMonth = endDate.getMonth();
-		let endDay = endDate.getDate();
-		let end = new Date(endYear, endMonth, endDay, 24, 0, 0, 0);
-		console.log('end', end);
+		endDate.setHours(24, 0, 0);
+		// let endYear = endDate.getFullYear();
+		// let endMonth = endDate.getMonth();
+		// let endDay = endDate.getDate();
+		// let end = new Date(endYear, endMonth, endDay, 24, 0, 0, 0);
 
 		result.push([
 			rotNumber,
@@ -32,8 +30,8 @@ export default function Timeline({ rotations }) {
 			)
 				.toString()
 				.slice(0, 11)}`,
-			start,
-			end,
+			startDate,
+			endDate,
 		]);
 	}
 	console.log('timeline', result);
@@ -42,28 +40,30 @@ export default function Timeline({ rotations }) {
 	console.log('resultArray', resultArray);
 	resultArray.forEach((e) => e.unshift(rotations.keys().next().value.slice(5)));
 	return (
-		<Chart
-			//width={'100%'}
-			// height={'200px'}
-			chartType='Timeline'
-			loader={<div>Loading Chart</div>}
-			data={[
-				[
-					{ type: 'string', id: 'Year' },
-					{ type: 'string', id: 'Rotation' },
-					{ type: 'string', role: 'tooltip' },
-					{ type: 'date', id: 'Start' },
-					{ type: 'date', id: 'End' },
-				],
-				...resultArray,
-			]}
-			options={{
-				timeline: { showRowLabels: false, showBarLabels: false },
-				height: 100,
-				//width: '80%',
-			}}
-			rootProps={{ 'data-testid': '4' }}
-		/>
+		<div id='chart_wrapper'>
+			<Chart
+				//width={'100%'}
+				// height={'200px'}
+				chartType='Timeline'
+				loader={<div>Loading Chart</div>}
+				data={[
+					[
+						{ type: 'string', id: 'Year' },
+						{ type: 'string', id: 'Rotation' },
+						{ type: 'string', role: 'tooltip' },
+						{ type: 'date', id: 'Start' },
+						{ type: 'date', id: 'End' },
+					],
+					...resultArray,
+				]}
+				options={{
+					timeline: { showRowLabels: false, showBarLabels: false },
+					height: 100,
+					width: 1400,
+				}}
+				rootProps={{ 'data-testid': '4' }}
+			/>
+		</div>
 	);
 }
 
